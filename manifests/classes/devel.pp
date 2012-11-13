8562b616d276acf70da990c2647732734e33e576
Jeff Graham <jeff@funnymonkey.com>
2012-11-12 16:49:43 -0800

    Move network related stuff to devel class rather than drupal

--- manifests/classes/devel.pp
+++ manifests/classes/devel.pp
@@ -1,18 +1,40 @@
 class devel {
-	package { screen: 
+	package { screen:
 		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package { mailutils: 
+	package { mailutils:
 		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package { vim: 
+	package { vim:
 		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package { vim-puppet: 
+	package { vim-puppet:
 		ensure => installed,
 		require => Exec['apt-update']
 	}
+
+	file { "/etc/network/if-up.d/logininfo":
+		ensure => "file",
+		replace => true,
+		owner  => 'root',
+    group  => 'root',
+    mode   => 0755,
+		content => "#!/bin/sh
+if [ \"\$METHOD\" = loopback ]; then
+  exit 0
+fi
+
+# only run from ifup.
+if [ \"\$MODE\" != start ]; then
+  exit 0
+fi
+
+ifconfig | awk -F: '/inet addr:/ {print \$2}' | grep -v \"127.0.0.1\" | awk '{ print \$1 }' > /etc/issue
+echo \"\" >> /etc/issue
+"
+	}
+
 }
--- manifests/classes/drupal.pp
+++ manifests/classes/drupal.pp
@@ -62,27 +62,6 @@ local_transport = error:local delivery is disabled",
 		require => Package['postfix']
 	}
 
-	file { "/etc/network/if-up.d/logininfo":
-		ensure => "file",
-		replace => true,
-		owner  => 'root',
-    group  => 'root',
-    mode   => 0755,
-		content => "#!/bin/sh
-if [ \"\$METHOD\" = loopback ]; then
-  exit 0
-fi
-
-# only run from ifup.
-if [ \"\$MODE\" != start ]; then
-  exit 0
-fi
-
-ifconfig | awk -F: '/inet addr:/ {print \$2}' | grep -v \"127.0.0.1\" | awk '{ print \$1 }' > /etc/issue
-echo \"\" >> /etc/issue
-"
-	}
-
 	#pecl install uploadprogress
 	exec {'pecl install uploadprogress':
 		command     => "/usr/bin/pecl install uploadprogress",
