bc76856ac211852e77c611ad8a48995f26260dee
Jeff Graham <jeff@funnymonkey.com>
2012-11-14 19:05:08 -0800

    Minor cleanup and refactoring

--- manifests/classes/drupal.pp
+++ manifests/classes/drupal.pp
@@ -5,56 +5,57 @@ class drupal {
 		require => Exec['apt-update']
 	}
 	class { 'mysql::server':
-        	config_hash => { 'root_password' => 'foo' },
+    config_hash => { 'root_password' => 'foo' },
 		require => Exec['apt-update']
-        }
+  }
 
 	package { php5:
 		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package {
-		php5-mysql: ensure => installed,
+	package { php5-mysql:
+		ensure => installed,
 		require => Exec['apt-update']
 	}
 	package { php5-imap:
 		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package {
-		php5-gd: ensure => installed,
+	package { php5-gd:
+		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package {
-		php5-dev: ensure => installed,
+	package { php5-dev:
+		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package {
-		php-pear: ensure => installed,
+	package { php-pear:
+		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package {
-		php5-curl: ensure => installed,
+	package { php5-curl:
+		ensure => installed,
 		require => Exec['apt-update']
 	}
 	package { php-apc:
 		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package {
-		php5-cli: ensure => installed,
+	package { php5-cli:
+		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package {
-		make: ensure => installed,
+	package { make:
+		ensure => installed,
 		require => Exec['apt-update']
 	}
-	package { postfix: ensure => installed,
+	package { postfix:
+		ensure => installed,
 		require => Exec['apt-update']
 	}
 
-	package {
-		ruby-compass: ensure => installed,
+	package { ruby-compass:
+		ensure => installed,
 		require => Exec['apt-update']
 	}
 
@@ -68,37 +69,21 @@ local_transport = error:local delivery is disabled",
 	}
 
 	#pecl install uploadprogress
-	exec {'pecl install uploadprogress':
+	exec { 'pecl install uploadprogress':
 		command     => "/usr/bin/pecl install uploadprogress",
 		require => Package['php-pear'],
 		creates => '/var/tmp/pecl-install-uploadprogress'
 	}
 
-	#echo "extension=uploadprogress.so" > /etc/php5/apache2/conf.d/uploadprogress.ini
-	exec {'enable uploadprogress':
+	exec { 'enable uploadprogress':
 		command => '/bin/echo "extension=uploadprogress.so" > /etc/php5/apache2/conf.d/uploadprogress.ini',
 		require => Class['apache::mod::php'],
 		creates => '/var/tmp/enable-uploadprogress'
 	}
 
-	## lucid has a deprecated comment tag in imap.ini
-	if $lsbdistcodename == 'lucid' {
-		exec {'enable imap comment tag':
-			command => "/bin/sed -i -e 's/# configuration for php Imap module/; configuration for php Imap module/' /etc/php5/cli/conf.d/imap.ini",
-			require => Package['php5-imap']
-		}
-
-		exec {'increase apc shm':
-			command => "/bin/echo 'apc.shm_size=\"64M\"' >> /etc/php5/apache2/conf.d/apc.ini",
-			require => Package['php-apc']
-		}
-	}
-
-	else {
-		exec {'increase apc shm':
-			command => "/bin/echo 'apc.shm_size=\"64M\"' >> /etc/php5/conf.d/apc.ini",
-			require => Package['php-apc']
-		}
+	exec { 'increase apc shm':
+		command => "/bin/echo 'apc.shm_size=\"64M\"' >> /etc/php5/conf.d/apc.ini",
+		require => Package['php-apc']
 	}
 
 	package { drush:
@@ -107,6 +92,11 @@ local_transport = error:local delivery is disabled",
 
 	package { 'libxml-xpath-perl':
 		ensure => installed
+	}
+
+	# create a directory
+	file { "/var/www":
+		ensure => "directory"
 	}
 
 	# Create a apache virtual host
@@ -129,11 +119,6 @@ local_transport = error:local delivery is disabled",
 			/usr/bin/wget -O /tmp/drupal-7.tgz -i -',
 		require => Package['libxml-xpath-perl'],
 		creates => '/var/tmp/drupal-downloaded'
-	}
-
-	# create a directory
-	file { "/var/www":
-		ensure => "directory"
 	}
 
 	exec {'unpack drupal':
