31d738a1505b51008af1ccb63bb948031a6a1aa3
Jeff Graham <jeff@funnymonkey.com>
2012-12-06 14:54:34 -0800

    Refactoring cleanup
    - Add dependency to php5 for php5-xdebug
    - Remove vim-puppet syntax highlighting
    - Remove file that set the console to the active IPs, vagrant is headless so this is unnecessary.

--- manifests/classes/devel.pp
+++ manifests/classes/devel.pp
@@ -8,37 +8,12 @@ class devel {
 	package { vim:
 		ensure => installed,
 	}
-	package { vim-puppet:
-		ensure => installed,
-	}
 	package { git:
 		ensure => installed,
 	}
-
 	package {
 		php5-xdebug:
 		ensure => installed,
+		require => Package['php5']
 	}
-
-	file { "/etc/network/if-up.d/logininfo":
-		ensure => "file",
-		replace => true,
-		owner  => 'root',
-    group  => 'root',
-    mode   => 0755,
-		content => "#!/bin/sh
-if [ \"\$METHOD\" = loopback ]; then
-  exit 0
-fi
-
-# only run from ifup.
-if [ \"\$MODE\" != start ]; then
-  exit 0
-fi
-
-ifconfig | awk -F: '/inet addr:/ {print \$2}' | grep -v \"127.0.0.1\" | awk '{ print \$1 }' > /etc/issue
-echo \"\" >> /etc/issue
-"
-	}
-
 }
