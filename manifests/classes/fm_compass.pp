852a7a76cf2fb6716fba19a02833cf89c6e0015c
Jeff Graham <jeff@dungeon.(none)>
2012-12-09 11:39:39 -0800

    Adding additional gems for compass

--- manifests/classes/fm_compass.pp
+++ manifests/classes/fm_compass.pp
@@ -1,10 +1,30 @@
 class fm_compass {
-	package { ruby-compass:
+	package { 'ruby-compass':
 		ensure => installed,
 	}
 
-	package { rubygems:
+	package { 'rubygems':
 		ensure => installed,
+	}
+
+	package { 'compass':
+		ensure => 'installed',
+		provider => 'gem',
+	}
+
+	package { 'sass':
+		ensure => 'installed',
+		provider => 'gem',
+	}
+
+	package { 'chunky_png':
+		ensure => 'installed',
+		provider => 'gem',
+	}
+
+	package { 'fssm':
+		ensure => 'installed',
+		provider => 'gem',
 	}
 
 	package { 'zurb-foundation':
