656ce1a28e0d1abe9b8a901980501669298f9eb5
Jeff Graham <jeff@funnymonkey.com>
2012-12-11 12:32:53 -0800

    Attempt to avoind shipped ruby/gem/puppet in /opt/vagrant_ruby

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,18 @@
+# Reset the path to avoid /opt/vagrant_ruby (we use the default packaged libs)
+file { '/etc/profile.d/vagrant_ruby.sh':
+  ensure => absent,
+}
+
+Exec { path => [
+    "/usr/local/sbin",
+    "/usr/local/bin",
+    "/usr/sbin",
+    "/usr/bin",
+    "/sbin:/bin",
+  ]
+}
+
 import "classes/*"
 import "nodes.pp"
+
+
