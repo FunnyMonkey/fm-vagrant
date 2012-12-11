a1b79ff5135ea0282972f7aaad4c551abe42f134
Jeff Graham <jeff@funnymonkey.com>
2012-12-11 12:30:02 -0800

    Make sure gems are installed in default ruby and not /opt/vagrant_ruby

--- manifests/classes/fm_compass.pp
+++ manifests/classes/fm_compass.pp
@@ -10,25 +10,30 @@ class fm_compass {
 	package { 'compass':
 		ensure => 'installed',
 		provider => 'gem',
+		require => Package['rubygems']
 	}
 
 	package { 'sass':
 		ensure => 'installed',
 		provider => 'gem',
+		require => Package['rubygems']
 	}
 
 	package { 'chunky_png':
 		ensure => 'installed',
 		provider => 'gem',
+		require => Package['rubygems']
 	}
 
 	package { 'fssm':
 		ensure => 'installed',
 		provider => 'gem',
+		require => Package['rubygems']
 	}
 
 	package { 'zurb-foundation':
     ensure   => 'installed',
     provider => 'gem',
+    require => Package['rubygems']
 	}
 }
