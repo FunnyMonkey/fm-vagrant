bbe1f939b2db3b877436d17fe7383bd7cc24d0d1
Jeff Graham <jeff@funnymonkey.com>
2013-03-25 15:44:56 -0700

    Fixing issue where outdated repositories halts initial bootup
    For those already using the previous versions you can manually ssh in
    and issue `sudo apt-get update` and then from the host OS issue
    vagrant provision.

--- build.sh
+++ build.sh
@@ -52,6 +52,7 @@ Vagrant::Config.run do |config|
   config.vm.host_name = '${vgrthostname}'
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.provision :shell, :inline => "/usr/bin/apt-get update"
   config.vm.provision :shell, :inline => "/usr/bin/apt-get install -y puppet libaugeas-ruby augeas-tools rubygems"
   config.vm.provision :puppet, :module_path => "modules", :options => "--verbose" do |puppet|
     puppet.manifests_path = "manifests"
