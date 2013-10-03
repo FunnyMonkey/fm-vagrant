0c1e6aac725f9ca458ca8f177cfe9baca97e5c1e
Stephen Brown II <Stephen.Brown2@gmail.com>
2013-10-03 13:33:12 -0400

    Update puppetlabs-mysql to 1.0.0, tweak fm_mysql.pp, and enhance #3 hack with meminbytes.rb
    Update puppetlabs-mysql to 1.0.0:
      This is the current latest released tag of the mysql module. In the future, the 2.x release
      will allow for custom parameters in my.cnf, as well as other enhancements, necessitating
      a rewrite of fm_mysql.pp, but also removes the necessity to hack the my.cnf.erb for #3.
    Tweak fm_mysql.pp:
      Fixed some whitespace (changed spaces to tabs to match the rest of the file,
      Changed grant['all'] to grant['ALL'] for sanity in fm_mysql.pp. The mismatched case caused
      puppet to change it back each time it ran, for example in a `vagrant provision`. Probably
      an edge case, but good for consistency now.
    Enhance #3 hack with meminbytes.rb:
      The hack for Issue #3 added a fixed value for innodb_buffer_pool_size. This enhanced hack
      uses a custom facter fact to find and use half the amount of memory available to the VM.
      This should be moved to the override_options hash when puppetlabs-mysql 2.0.0+ is used.
      meminbytes.rb source: https://blog.kumina.nl/2011/03/facter-facts-for-memory-in-bytes

--- modules/mysql/manifests/params.pp
+++ modules/mysql/manifests/params.pp
@@ -12,13 +12,38 @@
 #
 class mysql::params {
 
-  $bind_address        = '127.0.0.1'
-  $port                = 3306
-  $etc_root_password   = false
-  $ssl                 = false
+  $bind_address          = '127.0.0.1'
+  $client_package_ensure = 'present'
+  $config_template       = 'mysql/my.cnf.erb'
+  $default_engine        = 'UNSET'
+  $etc_root_password     = false
+  $expire_logs_days      = 10
+  $key_buffer            = '16M'
+  $manage_service        = true
+  $max_allowed_packet    = '16M'
+  $max_binlog_size       = '100M'
+  $max_connections       = 151
+  $myisam_recover        = 'BACKUP'
+  $old_root_password     = ''
+  $package_ensure        = 'present'
+  $port                  = 3306
+  $purge_conf_dir        = false
+  $query_cache_limit     = '1M'
+  $query_cache_size      = '16M'
+  $restart               = true
+  $root_password         = 'UNSET'
+  $ssl                   = false
+  $thread_cache_size     = 8
+  $thread_stack          = '256K'
+
+  # mysql::bindings
+  $java_package_ensure   = 'present'
+  $perl_package_ensure   = 'present'
+  $python_package_ensure = 'present'
+  $ruby_package_ensure   = 'present'
 
   case $::operatingsystem {
-    "Ubuntu": {
+    'Ubuntu': {
       $service_provider = upstart
     }
     default: {
@@ -28,47 +53,101 @@ class mysql::params {
 
   case $::osfamily {
     'RedHat': {
+      if $::operatingsystem == 'Fedora' and $::operatingsystemrelease >= 19 {
+        $client_package_name = 'mariadb'
+        $server_package_name = 'mariadb-server'
+      } else {
+        $client_package_name = 'mysql'
+        $server_package_name = 'mysql-server'
+      }
       $basedir               = '/usr'
-      $datadir               = '/var/lib/mysql'
-      $service_name          = 'mysqld'
-      $client_package_name   = 'mysql'
-      $server_package_name   = 'mysql-server'
-      $socket                = '/var/lib/mysql/mysql.sock'
-      $pidfile               = '/var/run/mysqld/mysqld.pid'
       $config_file           = '/etc/my.cnf'
+      $datadir               = '/var/lib/mysql'
       $log_error             = '/var/log/mysqld.log'
-      $ruby_package_name     = 'ruby-mysql'
-      $ruby_package_provider = 'gem'
-      $python_package_name   = 'MySQL-python'
-      $java_package_name     = 'mysql-connector-java'
+      $php_package_name      = 'php-mysql'
+      $pidfile               = '/var/run/mysqld/mysqld.pid'
       $root_group            = 'root'
+      $service_name          = 'mysqld'
+      $socket                = '/var/lib/mysql/mysql.sock'
       $ssl_ca                = '/etc/mysql/cacert.pem'
       $ssl_cert              = '/etc/mysql/server-cert.pem'
       $ssl_key               = '/etc/mysql/server-key.pem'
+      $tmpdir                = '/tmp'
+      # mysql::bindings
+      $java_package_name     = 'mysql-connector-java'
+      $perl_package_name     = 'perl-DBD-MySQL'
+      $python_package_name   = 'MySQL-python'
+      $ruby_package_name     = 'ruby-mysql'
+      $ruby_package_provider = 'gem'
+    }
+
+    'Suse': {
+      $basedir               = '/usr'
+      $config_file           = '/etc/my.cnf'
+      $client_package_name   = $::operatingsystem ? {
+        /OpenSuSE/           => 'mysql-community-server-client',
+        /(SLES|SLED)/        => 'mysql-client',
+        }
+      $datadir               = '/var/lib/mysql'
+      $log_error             = $::operatingsystem ? {
+        /OpenSuSE/           => '/var/log/mysql/mysqld.log',
+        /(SLES|SLED)/        => '/var/log/mysqld.log',
+      }
+      $pidfile               = $::operatingsystem ? {
+        /OpenSuSE/           => '/var/run/mysql/mysqld.pid',
+        /(SLES|SLED)/        => '/var/lib/mysql/mysqld.pid',
+      }
+      $root_group            = 'root'
+      $server_package_name   = $::operatingsystem ? {
+        /OpenSuSE/           => 'mysql-community-server',
+        /(SLES|SLED)/        => 'mysql',
+      }
+      $service_name          = 'mysql'
+      $socket                = $::operatingsystem ? {
+        /OpenSuSE/           => '/var/run/mysql/mysql.sock',
+        /(SLES|SLED)/        => '/var/lib/mysql/mysql.sock',
+      }
+      $ssl_ca                = '/etc/mysql/cacert.pem'
+      $ssl_cert              = '/etc/mysql/server-cert.pem'
+      $ssl_key               = '/etc/mysql/server-key.pem'
+      $tmpdir                = '/tmp'
+      # mysql::bindings
+      $java_package_name     = 'mysql-connector-java'
+      $perl_package_name     = 'perl-DBD-mysql'
+      $python_package_name   = 'python-mysql'
+      $ruby_package_name     = $::operatingsystem ? {
+        /OpenSuSE/           => 'rubygem-mysql',
+        /(SLES|SLED)/        => 'ruby-mysql',
+      }
     }
 
     'Debian': {
       $basedir              = '/usr'
-      $datadir              = '/var/lib/mysql'
-      $service_name         = 'mysql'
       $client_package_name  = 'mysql-client'
-      $server_package_name  = 'mysql-server'
-      $socket               = '/var/run/mysqld/mysqld.sock'
-      $pidfile              = '/var/run/mysqld/mysqld.pid'
       $config_file          = '/etc/mysql/my.cnf'
+      $datadir              = '/var/lib/mysql'
       $log_error            = '/var/log/mysql/error.log'
-      $ruby_package_name    = 'libmysql-ruby'
-      $python_package_name  = 'python-mysqldb'
-      $java_package_name    = 'libmysql-java'
+      $php_package_name     = 'php5-mysql'
+      $pidfile              = '/var/run/mysqld/mysqld.pid'
       $root_group           = 'root'
+      $server_package_name  = 'mysql-server'
+      $service_name         = 'mysql'
+      $socket               = '/var/run/mysqld/mysqld.sock'
       $ssl_ca               = '/etc/mysql/cacert.pem'
       $ssl_cert             = '/etc/mysql/server-cert.pem'
       $ssl_key              = '/etc/mysql/server-key.pem'
+      $tmpdir                = '/tmp'
+      # mysql::bindings
+      $java_package_name    = 'libmysql-java'
+      $perl_package_name    = 'libdbd-mysql-perl'
+      $python_package_name  = 'python-mysqldb'
+      $ruby_package_name    = 'libmysql-ruby'
     }
 
     'FreeBSD': {
       $basedir               = '/usr/local'
       $datadir               = '/var/db/mysql'
+      $tmpdir                = '/tmp'
       $service_name          = 'mysql-server'
       $client_package_name   = 'databases/mysql55-client'
       $server_package_name   = 'databases/mysql55-server'
@@ -76,35 +155,42 @@ class mysql::params {
       $pidfile               = '/var/db/mysql/mysql.pid'
       $config_file           = '/var/db/mysql/my.cnf'
       $log_error             = "/var/db/mysql/${::hostname}.err"
-      $ruby_package_name     = 'ruby-mysql'
-      $ruby_package_provider = 'gem'
-      $python_package_name   = 'databases/py-MySQLdb'
-      $java_package_name     = 'databases/mysql-connector-java'
+      $php_package_name      = 'php5-mysql'
       $root_group            = 'wheel'
       $ssl_ca                = undef
       $ssl_cert              = undef
       $ssl_key               = undef
+      # mysql::bindings
+      $java_package_name     = 'databases/mysql-connector-java'
+      $perl_package_name     = 'p5-DBD-mysql'
+      $python_package_name   = 'databases/py-MySQLdb'
+      $ruby_package_name     = 'ruby-mysql'
+      $ruby_package_provider = 'gem'
     }
 
     default: {
       case $::operatingsystem {
         'Amazon': {
           $basedir               = '/usr'
-          $datadir               = '/var/lib/mysql'
-          $service_name          = 'mysqld'
           $client_package_name   = 'mysql'
-          $server_package_name   = 'mysql-server'
-          $socket                = '/var/lib/mysql/mysql.sock'
           $config_file           = '/etc/my.cnf'
+          $datadir               = '/var/lib/mysql'
           $log_error             = '/var/log/mysqld.log'
-          $ruby_package_name     = 'ruby-mysql'
-          $ruby_package_provider = 'gem'
-          $python_package_name   = 'MySQL-python'
-          $java_package_name     = 'mysql-connector-java'
+          $php_package_name      = 'php-mysql'
           $root_group            = 'root'
+          $server_package_name   = 'mysql-server'
+          $service_name          = 'mysqld'
+          $socket                = '/var/lib/mysql/mysql.sock'
           $ssl_ca                = '/etc/mysql/cacert.pem'
           $ssl_cert              = '/etc/mysql/server-cert.pem'
           $ssl_key               = '/etc/mysql/server-key.pem'
+          $tmpdir                = '/tmp'
+          # mysql::bindings
+          $java_package_name     = 'mysql-connector-java'
+          $perl_package_name     = 'perl-DBD-MySQL'
+          $python_package_name   = 'MySQL-python'
+          $ruby_package_name     = 'ruby-mysql'
+          $ruby_package_provider = 'gem'
         }
 
         default: {
