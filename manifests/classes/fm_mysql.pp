18690a24af8e6fac76a2e02a9a30c64a741721fe
Jeff Graham <jeff@funnymonkey.com>
2014-04-09 09:25:02 -0700

    Update to newer mysql module and minimally setup server

--- manifests/classes/fm_mysql.pp
+++ manifests/classes/fm_mysql.pp
@@ -1,17 +1,7 @@
 class fm_mysql {
 	include mysql
 
-	class { 'mysql::server':
-    config_hash => { 'root_password' => 'foo' },
+	class { '::mysql::server':
+    'root_password' => 'foo',
   }
-
-  # install a database setting valid db permissions
-	# TODO: if we are going to be using this for building production VMs we will
-	# want to tighten this up.
-	mysql::db { 'drupal':
-		user     => 'drupal',
-		password => 'drupal',
-		host     => 'localhost',
-		grant    => ['all'],
-	}
 }
