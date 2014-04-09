031f090b11e6c760dc70e28dbecea747f581a3c4
Jeff Graham <jeff@funnymonkey.com>
2014-04-09 09:13:54 -0700

    Remove deprecated build scripts

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -e .vagrantfile ]
-  then
-    echo ".vagrantfile still exists. "
-    echo "First run vagrant destroy if you want to create a new environment."
-    exit
-fi
-rm Vagrantfile
-rm manifests/nodes.pp
